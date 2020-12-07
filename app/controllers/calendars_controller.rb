559d80ea78cb71e9e06c9cadf91ff2e8c2c0c102
hondakeisuke <keisuke.h101@icloud.com>
2020-12-07 16:03:16 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
@@ -38,5 +38,5 @@ class CalendarsController < ApplicationController
       @week_days.push(days)
     end
 
-  end
+  end 
 end
